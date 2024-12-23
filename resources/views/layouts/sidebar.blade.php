b46d5156cbb5f442d34aa8a31a7fd6ca1d6e9c74
Michael <darundaymichael4@gmail.com>
2024-12-23 15:31:05 +0800

    basta

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -81,7 +81,7 @@
                     </svg>
                     <span class="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center">3</span>
                 </div>
-                <span>Notifications</span>
+                <span>&nbsp;&nbsp;Notifications</span>
             </button>
 
             <!-- Notification Slide-out Panel -->
